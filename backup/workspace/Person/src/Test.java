d18777e1214ea82e86ad5562d052d68fa51a4736
hoshikagi96 <hoshikagi96@gmail.com>
2017-12-25 17:24:37 +0900

    12/25

--- backup/workspace/Person/src/Test.java
+++ backup/workspace/Person/src/Test.java
@@ -2,45 +2,47 @@
 public class Test {
 	public static void main(String[] args){
 		Person taro=new Person();
-		taro.name="taro";
-		taro.age=18;
+		taro.name="山田太郎";
+		taro.age=20;
+		taro.phoneNumber="03-1234-5678";
+		taro.address="東京都大田区";
 		System.out.println(taro.name);
 		System.out.println(taro.age);
-		
-		Person jiro=new Person("jiro",20);
+		System.out.println(taro.phoneNumber);
+		System.out.println(taro.address);
+		taro.talk();
+		taro.walk();
+		taro.run();
+
+		Person jiro=new Person();
+		jiro.name="木村次郎";
+		jiro.age=18;
+		jiro.phoneNumber="03-9876-5432";
+		jiro.address="東京都板橋区";
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
-		
-		Person saburo=new Person("saburo");
-		System.out.println(saburo.name);
-		System.out.println(saburo.age);
-		
-		Person noname=new Person(25);
-		System.out.println(noname.name);
-		System.out.println(noname.age);
-		
-		Person hanako=new Person(17,"hanako");
+		System.out.println(jiro.phoneNumber);
+		System.out.println(jiro.address);
+
+		Person hanako=new Person();
+		hanako.name="鈴木花子";
+		hanako.age=16;
+		hanako.phoneNumber="03-8765-4321";
+		hanako.address="東京都墨田区";
 		System.out.println(hanako.name);
 		System.out.println(hanako.age);
-		
-	    
-		System.out.print("\n");
-		Robot aibo=new Robot();
-	    aibo.name="AIBO";
-	    aibo.talk();
-	    System.out.print("\n");
-	    
-	    Robot asimo=new Robot();
-	    asimo.name="ASIMO";
-	    asimo.walk();
-	    
-	    Robot pepper=new Robot();
-	    pepper.name="ペッパー";
-	    pepper.run();
-	    
-	    Robot doraemon=new Robot();
-	    doraemon.name="ドラえもん";
-	    doraemon.sing();
+		System.out.println(hanako.phoneNumber);
+		System.out.println(hanako.address);
+
+		Person miho=new Person();
+		miho.name="高橋美穂";
+		miho.age=22;
+		miho.phoneNumber="03-0123-4567";
+		miho.address="東京都葛飾区";
+		System.out.println(miho.name);
+		System.out.println(miho.age);
+		System.out.println(miho.phoneNumber);
+		System.out.println(miho.address);
 	}
 
 }
---
+++ backup/workspace/Robot/src/Test.java
@@ -0,0 +1,27 @@
+
+public class Test {
+	public static void main(String[] args){
+	Robot aibo=new Robot();
+	aibo.name="AIBO";
+	
+	Robot asimo=new Robot();
+	asimo.name="ASIMO";
+	
+	Robot pepper=new Robot();
+	pepper.name="ペッパー";
+	
+	Robot doraemon=new Robot();
+	doraemon.name="ドラえもん";
+	
+	aibo.talk();
+	aibo.walk();
+	aibo.run();
+	
+	asimo.talk();
+	asimo.walk();
+	asimo.run();
+	
+	
+	}		
+
+}
